275426841d4398b508f1265047e4a9bca877dc1d
Simon <sitag20@student.sdu.dk>
2022-08-22 17:32:21 +0200

    last commit

--- scripts/cleanupTestEnvironment.sh
+++ scripts/cleanupTestEnvironment.sh
@@ -2,15 +2,16 @@
 
 name=${docker_image}-test-${docker_run_id}
 
-echo "docker_image: ${docker_image}, docker_run_id: ${docker_run_id}, name: ${name}"
+echo "Docker image name: ${name}"
 
-[[ -z "${docker_image}" ]] && exit 1
-[[ -z "${docker_run_id}" ]] && exit 1
+[[ -z "${docker_image}" ]] && echo "missing docker_image argument" && exit 1
+[[ -z "${docker_run_id}" ]] && echo "missing docker_id argument" && exit 1
 
 if [ ! -z "$(sudo -u ubuntu kubectl get service | grep ${name})" ]; then
     sudo -u ubuntu kubectl delete service ${name}
     [[ "$?" != "0" ]] && exit 1
 fi
+
 if [ ! -z "$(sudo -u ubuntu kubectl get deployment | grep ${name})" ]; then
     sudo -u ubuntu kubectl delete deployment ${name}
     exit $?
--- scripts/deployToProd.sh
+++ scripts/deployToProd.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
+
 if [ -z "$(sudo -u ubuntu kubectl get deployment | grep deployment-backend)" ]; then
     sudo -u ubuntu kubectl create -f manifest/deployment-backend.yaml
 else
-    echo "docker_tag: ${docker_tag}"
-    [[ -z "${docker_tag}" ]] && exit 1
+    [[ -z "${docker_tag}" ]] && echo "docker_tag: ${docker_tag}"  && exit 1
+    [[ -z "${docker_id}" ]] && echo "docker_id: ${docker_id}" && exit 1
 
-    sudo -u ubuntu kubectl set image deployments/deployment-backend go_backend=jedie20/go_backend:${docker_tag}
+    sudo -u ubuntu kubectl set image deployments/deployment-backend ${docker_id}/go_backend:${docker_tag}
 fi
 [[ "$?" != "0" ]] && exit 1
 
 sudo -u ubuntu kubectl apply -f manifest/service-backend.yaml
 [[ "$?" != "0" ]] && exit 1
 
-# =======================
-
 if [ -z "$(sudo -u ubuntu kubectl get deployment | grep deployment-frontend)" ]; then
     sudo -u ubuntu kubectl create -f manifest/deployment-frontend.yaml
 else
-    echo "docker_tag: ${docker_tag}"
-    [[ -z "${docker_tag}" ]] && exit 1
+    [[ -z "${docker_tag}" ]] && echo "docker_tag: ${docker_tag}"  && exit 1
+    [[ -z "${docker_id}" ]] && echo "docker_id: ${docker_id}" && exit 1
 
-    sudo -u ubuntu kubectl image deployments/deployment-frontend go_backend=jedie20/frontend:${docker_tag}
+    sudo -u ubuntu kubectl image deployments/deployment-frontend ${docker_id}/frontend:${docker_tag}
 fi
 [[ "$?" != "0" ]] && exit 1
 
